bc2eb4aee0a976699a6f56de1e946816bd5a3ac0
github user name <yokota.tomo222282@gmail.com>
2023-02-11 14:51:18 +0900

    com

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -3,25 +3,15 @@ import { createSlice } from '@reduxjs/toolkit';
 export const userSlice = createSlice({
   name: 'user',
   initialState: {
-    // user: JSON.parse(localStorage.getItem('user')) || null,
-    user: {
-      _id: '63e5a22b273d540ab10bb7f2',
-      username: '来てくれたあなた',
-      email: 'u@u',
-      password: '123456',
-      profileImg: '',
-      isAdmin: false,
-      createdAt: '2023-02-10T01:47:23.692Z',
-      updatedAt: '2023-02-10T01:47:23.692Z',
-    },
+    user: JSON.parse(localStorage.getItem('user')) || null,
   },
   reducers: {
     login: (state, action) => {
       state.user = action.payload;
     },
-    // logout: (state) => {
-    //   state.user = null;
-    // },
+    logout: (state) => {
+      state.user = null;
+    },
     followEvent: (state, action) => {
       state.user = action.payload;
     },
